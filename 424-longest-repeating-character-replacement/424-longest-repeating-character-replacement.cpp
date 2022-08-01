8c28dbd8e27401dfa6e855cfef4d45e553db94ce
namnanaman <78403255+namnanaman@users.noreply.github.com>
2022-08-01 16:47:13 +0530

    Time: 16 ms (61.59%), Space: 7 MB (51.68%) - LeetHub

--- 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.cpp
+++ 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.cpp
@@ -2,6 +2,26 @@ class Solution {
 public:
     int characterReplacement(string s, int k) {
         
+//         vector<int> freq(26);
+//         int mostFreqLetter = 0;
+//         int left = 0;
+//         int maxi = 0;
+        
+//         for(int right = 0;right < s.size();right++){
+//             freq[s[right] - 'A']++;
+//             mostFreqLetter = max(mostFreqLetter,freq[s[right] - 'A']);
+            
+//             int lettersToChange = (right - left + 1) - mostFreqLetter;
+//             while(lettersToChange > k){
+//                 freq[s[left] - 'A']--;
+//                 left--;
+//             }
+//             maxi = max(maxi,right - left + 1);
+//         }
+        
+//         return maxi;
+        
+        
         
         vector<int>  freq(26);
         int mostFreqLetter = 0;
