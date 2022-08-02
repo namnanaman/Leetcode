d367165381ab5920481e237502051dcf937b5000
namnanaman <78403255+namnanaman@users.noreply.github.com>
2022-08-02 22:19:52 +0530

    Time: 266 ms (45.22%), Space: 71.2 MB (69.71%) - LeetHub

--- 904-fruit-into-baskets/904-fruit-into-baskets.cpp
+++ 904-fruit-into-baskets/904-fruit-into-baskets.cpp
@@ -3,11 +3,10 @@ public:
     int totalFruit(vector<int>& fruits) {
         unordered_map<int,int> m;
         int i =0,j=0,ans=0;
-        
-        while(j < fruits.size()){
+        for(int j = 0;j < fruits.size();j++){
             m[fruits[j]]++;
             
-            while(m.size() > 2){
+            if(m.size() > 2){
                 if(m[fruits[i]] == 1){
                     m.erase(fruits[i]);
                 }else{
@@ -16,8 +15,24 @@ public:
                 i++;
             }
             ans = max(ans,j-i+1);
-            j++;
+            
+            
         }
+        
+//         while(j < fruits.size()){
+//             m[fruits[j]]++;
+            
+//             while(m.size() > 2){
+//                 if(m[fruits[i]] == 1){
+//                     m.erase(fruits[i]);
+//                 }else{
+//                     m[fruits[i]]--;
+//                 }
+//                 i++;
+//             }
+//             ans = max(ans,j-i+1);
+//             j++;
+//         }
         return ans;
     }        
     
