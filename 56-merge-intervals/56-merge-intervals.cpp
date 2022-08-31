8b40b5996ba81bb34a71f4180e5a6df7b138133a
namnanaman <78403255+namnanaman@users.noreply.github.com>
2022-08-31 16:44:36 +0530

    Time: 78 ms (27.53%), Space: 19.1 MB (44.55%) - LeetHub

---
+++ 56-merge-intervals/56-merge-intervals.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        vector<vector<int>> ans;
+        
+        sort(intervals.begin(),intervals.end());
+        ans.push_back(intervals[0]);
+        
+        for(int i =1 ;i < intervals.size();i++){
+            if(ans.back()[1] < intervals[i][0]){
+                ans.push_back(intervals[i]);
+            }else{
+                ans.back()[1] = max(ans.back()[1],intervals[i][1]);
+            }
+        }
+        return ans;
+    }
+};
