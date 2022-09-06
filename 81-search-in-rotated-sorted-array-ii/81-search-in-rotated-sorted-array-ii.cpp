6401df4f115ec73f0a5c618e4bba414cda7c4e37
namnanaman <78403255+namnanaman@users.noreply.github.com>
2022-09-06 14:58:10 +0530

    Time: 9 ms (61.13%), Space: 14 MB (74.34%) - LeetHub

--- 81-search-in-rotated-sorted-array-ii/81-search-in-rotated-sorted-array-ii.cpp
+++ 81-search-in-rotated-sorted-array-ii/81-search-in-rotated-sorted-array-ii.cpp
@@ -5,13 +5,14 @@ public:
         int high = nums.size() - 1;
         
         while(low <= high){
+            while(low < high && nums[low] == nums[low + 1]) low++;
+            while(low < high && nums[high] == nums[high - 1] ) high--;
+            
             int mid = low + (high-low)/2;
             
             if(nums[mid] == target) return true;
-            else if(nums[low] == nums[mid] && nums[high] == nums[mid]) {
-                low++;
-                high--;
-            }else if(nums[low] <= nums[mid]){
+           
+            else if(nums[low] <= nums[mid]){
                 if(target >= nums[low] && target < nums[mid]){
                     high = mid -1 ;
                 }else low = mid + 1;
