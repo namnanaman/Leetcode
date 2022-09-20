84d75911bc5de6ff3018eff3746c2ebe67adc752
namnanaman <78403255+namnanaman@users.noreply.github.com>
2022-09-20 11:08:40 +0530

    Time: 0 ms (100.00%), Space: 6 MB (48.06%) - LeetHub

---
+++ 1922-count-good-numbers/1922-count-good-numbers.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int countGoodNumbers( long n) {
+        long long result = n%2 == 0 ? 1 : 5 ;
+        long long x = 20;
+        int MOD = 1000000007;
+        for(long long  i = n/2;i > 0;i/=2){
+            if(i%2!= 0) result = result *x %MOD;
+            
+            x = x*x %MOD;
+        }
+        return (int)result;
+    }
+};
