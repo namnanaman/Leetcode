0b8a837e622d19895f8a4176adff04761c714501
namnanaman <78403255+namnanaman@users.noreply.github.com>
2022-09-28 12:11:18 +0530

    Time: 40 ms (25.88%), Space: 15 MB (16.96%) - LeetHub

--- 139-word-break/139-word-break.cpp
+++ 139-word-break/139-word-break.cpp
@@ -1,11 +1,13 @@
 class Solution {
- unordered_map<int,bool> mem;
+    public:
+
+    unordered_map<int,bool> mem;
     unordered_set<string> myset;//dictionary
     bool partition(string s,int pos){
         if(pos==s.size())   return true;
         if(mem.count(pos))  return mem[pos];
         
-        string t="";
+        
         for(int i=pos;i<s.size();++i)
             if(myset.count(s.substr(pos,i-pos+1)) and partition(s,i+1))
                 return mem[pos] = true;
