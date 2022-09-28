17f3f1b490d85c7876dbfcaf3908fca40c57a696
namnanaman <78403255+namnanaman@users.noreply.github.com>
2022-09-28 12:03:34 +0530

    Time: 53 ms (15.57%), Space: 14.9 MB (17.68%) - LeetHub

---
+++ 139-word-break/139-word-break.cpp
@@ -0,0 +1,21 @@
+class Solution {
+ unordered_map<int,bool> mem;
+    unordered_set<string> myset;//dictionary
+    bool partition(string s,int pos){
+        if(pos==s.size())   return true;
+        if(mem.count(pos))  return mem[pos];
+        
+        string t="";
+        for(int i=pos;i<s.size();++i)
+            if(myset.count(s.substr(pos,i-pos+1)) and partition(s,i+1))
+                return mem[pos] = true;
+        return mem[pos] = false;
+    }
+public:
+    bool wordBreak(string s, vector<string>& wordDict) {
+        for(auto str: wordDict)
+            myset.insert(str);
+        
+        return partition(s,0);
+    }
+};
