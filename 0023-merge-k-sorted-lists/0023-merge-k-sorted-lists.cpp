6b22e587b722afb1a0c4a28930a5f18c55f20919
namnanaman <78403255+namnanaman@users.noreply.github.com>
2022-11-02 12:26:31 +0530

    Time: 56 ms (42.98%), Space: 13.3 MB (75.71%) - LeetHub

---
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
@@ -0,0 +1,92 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class cmp
+{
+    public:
+    bool operator()(ListNode *a, ListNode *b)
+    {
+        return a->val > b->val;
+    }
+};
+
+
+class Solution {
+public:
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        priority_queue<ListNode *, vector<ListNode *>, cmp> q;
+        ListNode *dummy = new ListNode(-1);
+        auto *tail = dummy;
+        for(int i=0; i<lists.size(); i++)
+        {
+            if(lists[i])
+            {
+                q.push(lists[i]);
+            }
+        }
+        while(!q.empty())
+        {
+            ListNode *temp = q.top();
+            tail->next = temp;
+            tail = temp;
+            q.pop();
+            if(temp->next)
+            {
+                q.push(temp->next);
+            }
+        }
+        
+        auto *newHead = dummy->next;
+        auto *del = dummy;
+        delete dummy;
+        return newHead;
+    }
+};
+// class Compare{
+//     public:
+//     bool operator()(ListNode* a ,ListNode* b){
+//         return a->val > b->val;
+//     }
+// };
+
+// class Solution {
+// public:
+    
+//     ListNode* mergeKLists(vector<ListNode*>& lists) {
+        
+//         ListNode* dummy = new ListNode(-1);
+//         ListNode* tail = dummy;
+        
+//         priority_queue<ListNode*, vector<ListNode*>,Compare> q;
+        
+//         for(int i =0;i < lists.size();i++){
+//             q.push(lists[i]);
+//         }
+        
+        
+//         while(!q.empty())
+//         {
+//             ListNode *temp = q.top();
+//             tail->next = temp;
+//             tail = temp;
+//             q.pop();
+//             if(temp->next)
+//             {
+//                 q.push(temp->next);
+//             }
+//         } 
+//         return dummy->next;
+        
+        
+        
+        
+        
+//     }
+// };
